62c51aa028ebaeb89960cd7e86df27aaa2f40087
Lee HyunJoo <wayhome250@gmail.com>
2017-10-10 23:34:15 +0900

    django_filters 필터대상 추가

--- search/filters.py
+++ search/filters.py
@@ -1,8 +1,15 @@
-from django.contrib.auth.models import User
+from django import forms
+from django.contrib.auth.models import User, Group
 import django_filters
 
 
 class UserFilter(django_filters.FilterSet):
+    first_name = django_filters.CharFilter(lookup_expr="icontains")
+    year_joind = django_filters.NumberFilter(name='date_joined', lookup_expr='year')
+    year_joind_gt = django_filters.NumberFilter(name='date_joined', lookup_expr='year__gt')
+    year_joind_lt = django_filters.NumberFilter(name='date_joined', lookup_expr='year__lt')
+    groups = django_filters.ModelMultipleChoiceFilter(queryset=Group.objects.all(), widget=forms.CheckboxSelectMultiple)
+
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name']
+        fields = ['username', 'first_name', 'last_name', 'groups']
