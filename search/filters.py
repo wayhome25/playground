7f8eafcd2a905d1c624d1507ec1ab516898ebca1
Lee HyunJoo <wayhome250@gmail.com>
2017-10-10 23:38:19 +0900

    django_filters 딕셔너리를 활용하여 필드 정의

--- search/filters.py
+++ search/filters.py
@@ -4,12 +4,19 @@ import django_filters
 
 
 class UserFilter(django_filters.FilterSet):
-    first_name = django_filters.CharFilter(lookup_expr="icontains")
-    year_joind = django_filters.NumberFilter(name='date_joined', lookup_expr='year')
-    year_joind_gt = django_filters.NumberFilter(name='date_joined', lookup_expr='year__gt')
-    year_joind_lt = django_filters.NumberFilter(name='date_joined', lookup_expr='year__lt')
-    groups = django_filters.ModelMultipleChoiceFilter(queryset=Group.objects.all(), widget=forms.CheckboxSelectMultiple)
+    # first_name = django_filters.CharFilter(lookup_expr="icontains")
+    # year_joind = django_filters.NumberFilter(name='date_joined', lookup_expr='year')
+    # year_joind_gt = django_filters.NumberFilter(name='date_joined', lookup_expr='year__gt')
+    # year_joind_lt = django_filters.NumberFilter(name='date_joined', lookup_expr='year__lt')
+    # groups = django_filters.ModelMultipleChoiceFilter(queryset=Group.objects.all(), widget=forms.CheckboxSelectMultiple)
 
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'groups']
+        # fields = ['username', 'first_name', 'last_name', 'groups']
+        fields = {
+            'username': ['exact'],
+            'first_name': ['icontains'],
+            'last_name': ['exact'],
+            'date_joined': ['year', 'year__gt', 'year__lt'],
+            'groups': ['exact']
+        }
